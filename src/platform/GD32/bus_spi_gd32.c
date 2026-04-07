7f599aa7db35f558a5f805808ada72ad50f6e263
Jianping Wu <jianping.wu@gigadevice.com>
2026-04-07 18:55:17 +0800

    update GD32F4 spi driver

--- src/platform/GD32/bus_spi_gd32.c
+++ src/platform/GD32/bus_spi_gd32.c
@@ -248,7 +248,14 @@ void spiInternalStartDMA(const extDevice_t *dev)
 
         dma_channel_subperipheral_select((uint32_t)(dmaTx->dma), dmaTx->stream, dmaTx->channel);
 
-        xDMA_Init(streamRegsRx, (dma_general_config_struct *)dev->bus->dmaInitRx);
+        dma_general_config_struct * dmaGenerInitRx = dev->bus->dmaInitRx;
+        xDMA_Init(streamRegsRx, dmaGenerInitRx);
+        /* For GD32F4, peripheral width must be 8-bit */
+        if(dmaGenerInitRx->config.init_struct_s.periph_memory_width == DMA_PERIPH_WIDTH_16BIT) {
+            if(dmaGenerInitRx->data_mode == DMA_DATA_MODE_SINGLE) {
+                dma_periph_width_config((uint32_t)(dmaRx->dma), dmaRx->stream, DMA_PERIPH_WIDTH_8BIT);
+            }
+        }
 
         dma_channel_subperipheral_select((uint32_t)(dmaRx->dma), dmaRx->stream, dmaRx->channel);
 
